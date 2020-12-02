e5578f45aad3e29ae3fa00ced51df97815c342f3
ozgur <ozgurgurcan1996@gmail.com>
2020-12-03 00:22:54 +0300

    final update

--- init.py
+++ init.py
@@ -1,22 +1,76 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
 data = pd.read_excel(r'ilmesafe.xlsx')
-
 df = pd.DataFrame(data)
-##print (df[df.columns[1]])
 
-for columnIndex in range(2, 83):
-    column=df[df.columns[columnIndex]]
-    for rowIndex in range(1, 81):
-        print (column[rowIndex])
+def getCityName(cityIndex):
+    return df[df.columns[1]][cityIndex-1]
 
-print("bitti")
-# distanceBetweenStates = findDistance(currentState,destinationState)
 
-T=30
-factor =0.99
-T_init=T
-for i in range(1000):
-    print(i, 'cost=',cost0)
-    
-    T=T+factor
-    for j=in range(500):
+def getCityNames(cityList):
+    cityName=[]
+    for city in cityList:
+        if city == 82:
+            pass
+        else:
+            cityName.append(getCityName(city))
+    return cityName
+
+
+def getDistance(city1Index, city2Index):
+    if city1Index >= 1 and city1Index <= 82 and city2Index >= 1 and city2Index <= 82:
+        # print(getCityName(city1Index) +' to '+getCityName(city2Index))
+        return df[df.columns[city1Index+1]][city2Index-1]
+    else:
+        print('one of the cities is out of range (1-81)')
+        return -999999
+
+
+def getTotalDistance(cities):
+    totalDis = 0
+    index = 1
+    for first, second in zip(cities[:-1], cities[1:]):
+        totalDis += getDistance(first, second)
+        # print(totalDis)
+        # print(index)
+        index = index+1
+    totalDis += getDistance(cities[len(cities)-1],cities[0])    
+    return totalDis
+
+# Simulated annealing algorithm
+cityList = [x for x in range(82)] 
+cityList = cityList[1:]
+# cityList[1] , cityList[35] = cityList[35] , cityList[1]
+# print(getCityName(cityList[1]))
+# print(getCityName(cityList[35]))
+
+cost0 = getTotalDistance(cityList)
+print('total distance for Turkey : ', cost0)
+
+
+T = 100
+factor = 0.95
+T_init = T
+for i in range(100):
+    print(i, 'cost=', cost0, ' T=', T)
+
+    T = T*factor
+    for j in range(5000):
+        r1, r2 = np.random.randint(0,len(cityList),2)
+        # print(r1,r2)
+        cityList[r1], cityList[r2]=cityList[r2],cityList[r1]
+        newCost=getTotalDistance(cityList)
+        if newCost<cost0:
+            cost0=newCost
+        else:
+            x = np.random.uniform()
+            if x < np.exp((cost0-newCost)/T):
+                cost0 = newCost
+            else:
+                cityList[r1], cityList[r2]=cityList[r2],cityList[r1]   
+            
+print(cost0)   
+print(cityList)
+print(getCityNames(cityList))
