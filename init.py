7754cca7c7202f4dc76bffaad8b741725038e903
Fulde_hp <fulden.dogan@gmail.com>
2020-12-02 13:49:18 +0300

    update

--- init.py
+++ init.py
@@ -1,8 +1,22 @@
 import pandas as pd
 data = pd.read_excel(r'ilmesafe.xlsx')
-ilListesi = pd.DataFrame(data, columns= ['İL ADI'])
-print (ilListesi)
 
+df = pd.DataFrame(data)
+##print (df[df.columns[1]])
 
+for columnIndex in range(2, 83):
+    column=df[df.columns[columnIndex]]
+    for rowIndex in range(1, 81):
+        print (column[rowIndex])
 
+print("bitti")
 # distanceBetweenStates = findDistance(currentState,destinationState)
+
+T=30
+factor =0.99
+T_init=T
+for i in range(1000):
+    print(i, 'cost=',cost0)
+    
+    T=T+factor
+    for j=in range(500):
